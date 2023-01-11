1b8601713bc121076790892cbf680c6d550bf907
dlqudtjs <dlqudtjs730@naver.com>
2023-01-11 22:21:37 +0900

    으라차차

---
+++ Step08/No_4/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] prime = new boolean[N + 1];
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (prime[i]) {
+                continue;
+            }
+
+            for (int j = i * i; j <= N; j += i) {
+                prime[j] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i < prime.length; i++) {
+            if (!prime[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}

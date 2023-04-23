649ef354146055abaee30d0fa92402630408af87
dlqudtjs <51438119+dlqudtjs@users.noreply.github.com>
2023-04-23 13:48:36 +0900

    로봇 청소기 완

--- Backjoon question/No_14503 로봇 청소기/14503/src/Main.java
+++ Backjoon question/No_14503 로봇 청소기/14503/src/Main.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = new int[] { 0, -1, 0, 1 }; // 북 서 남 동
+    static int[] dx = new int[] { 0, 1, 0, -1 }; // 북 동 남 서
     static int[] dy = new int[] { -1, 0, 1, 0 };
     static int[][] map;
     static int N, M;
+    static int cnt = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,36 +32,35 @@ public class Main {
             }
         }
 
-        int cnt = bfs(r, c, d);
+        dfs(r, c, d);
 
         System.out.println(cnt);
     }
 
-    public static int bfs(int r, int c, int d) {
-        Queue<int[]> queue = new LinkedList<int[]>();
-        queue.add(new int[] { r, c });
-        map[r][c] = 1;
-        int cnt = 1;
+    public static void dfs(int r, int c, int d) {
+        if (map[r][c] == 0) { // 청소가 되있지 않다면 청소한다.
+            map[r][c] = 2;
+            cnt++;
+        }
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int nowX = now[0];
-            int nowY = now[1];
+        for (int i = 0; i < 4; i++) {
+            d = (d - 1) == -1 ? 3 : d - 1; // 반시계 방향으로 회전 (바라보는 왼쪽)
+            int nr = r + dy[d];
+            int nc = c + dx[d];
 
-            for (int i = 0; i < 4; i++) {
-                int x = nowX + dx[i];
-                int y = nowY + dy[i];
-
-                if (x < 0 || x >= M || y < 0 || y >= N || map[x][y] == 1) {
-                    continue;
-                }
-
-                queue.add(new int[] { x, y });
-                map[x][y] = 1;
-                cnt++;
+            if (map[nr][nc] == 0) {
+                dfs(nr, nc, d);
+                return;
             }
         }
 
-        return cnt;
+        // 후진
+        int nd = (d + 2) % 4;
+        int nr = r + dy[nd];
+        int nc = c + dx[nd];
+
+        if (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] != 1) {
+            dfs(nr, nc, d);
+        }
     }
 }

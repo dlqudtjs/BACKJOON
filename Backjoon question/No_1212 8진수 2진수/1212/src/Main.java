d6d3ca122909c87295b5a7395e893cb6ee3442d3
dlqudtjs <51438119+dlqudtjs@users.noreply.github.com>
2023-04-28 17:00:18 +0900

    8진수 2진수

---
+++ Backjoon question/No_1212 8진수 2진수/1212/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = br.readLine();
+
+        if (n.equals("0")) {
+            System.out.println(0);
+            return;
+        }
+
+        String[] binary = new String[] { "000", "001", "010", "011", "100", "101", "110", "111" };
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n.length(); i++) {
+            sb.append(binary[n.charAt(i) - '0']);
+        }
+
+        System.out.println(sb.substring(sb.indexOf("1")));
+    }
+}

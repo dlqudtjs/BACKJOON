38abecfde169d47121ec9cd84bc5951d3058e062
dlqudtjs <51438119+dlqudtjs@users.noreply.github.com>
2023-05-06 12:31:59 +0900

    수 찾기

---
+++ Backjoon question/No_1920 수 찾기/1920/src/Main.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            if (binarySearch(arr, key) != -1) {
+                sb.append("1").append("\n");
+                continue;
+            }
+
+            sb.append("0").append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (key < arr[mid]) {
+                high = mid - 1;
+                continue;
+            }
+            if (key > arr[mid]) {
+                low = mid + 1;
+                continue;
+            }
+            // key 값과 mid 값이 같을 경우
+            return mid;
+
+        }
+
+        return -1;
+    }
+}

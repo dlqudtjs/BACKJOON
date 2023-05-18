3df8c3caa60275f5ef2f832eea66f9458d68399e
dlqudtjs <51438119+dlqudtjs@users.noreply.github.com>
2023-05-19 00:21:08 +0900

    N과 M (1)

---
+++ Backjoon question/No_15649 N과 M/15649/src/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visited = new boolean[N];
+
+        dfs(0);
+
+        System.out.println(sb);
+
+    }
+
+    private static void dfs(int index) {
+        if (index == M) {
+            for (int n : arr) {
+                sb.append(n).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+            arr[index] = i + 1;
+            dfs(index + 1);
+            visited[i] = false;
+        }
+    }
+}
